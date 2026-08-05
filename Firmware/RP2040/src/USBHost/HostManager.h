c9a8c5ae02896da28c703c80f2cc6288e6235c2b
quizerno <43489654+quizerno@users.noreply.github.com>
2026-08-05 01:31:26 -0400

    Refactor is_hid_keyboard function in HostManager.h

--- Firmware/RP2040/src/USBHost/HostManager.h
+++ Firmware/RP2040/src/USBHost/HostManager.h
@@ -1165,22 +1165,22 @@ private:
 // };
 
 // #endif // _HOST_MANAGER_H_
-+
-+	bool is_hid_keyboard(const uint8_t* report_desc, uint16_t desc_len)
-+	{
-+		// Typical boot protocol keyboard descriptor starts with: Usage Page (Generic Desktop), Usage (Keyboard)
-+		std::array<uint8_t, 6> start_bytes = { 0x05, 0x01, 0x09, 0x06, 0xA1, 0x01 };
-+		if (desc_len < start_bytes.size())
-+		{
-+			return false;
-+		}
-+		for (size_t i = 0; i < start_bytes.size(); ++i)
-+		{
-+			if (report_desc[i] != start_bytes[i])
-+			{
-+				return false;
-+			}
-+		}
-+		return true;
-+	}
+ 
+ 	bool is_hid_keyboard(const uint8_t* report_desc, uint16_t desc_len)
+ 	{
+ 		// Typical boot protocol keyboard descriptor starts with: Usage Page (Generic Desktop), Usage (Keyboard)
+ 		std::array<uint8_t, 6> start_bytes = { 0x05, 0x01, 0x09, 0x06, 0xA1, 0x01 };
+ 		if (desc_len < start_bytes.size())
+ 		{
+ 			return false;
+ 		}
+ 		for (size_t i = 0; i < start_bytes.size(); ++i)
+ 		{
+ 			if (report_desc[i] != start_bytes[i])
+ 			{
+ 				return false;
+ 			}
+ 		}
+ 		return true;
+ 	}
 *** End Patch
